b453836994d8af2f160a0075a02a6dd99255323b
zzzzzuuuuu <agu0830@naver.com>
2023-07-11 21:31:13 +0900

    chore: 컴포넌트 분리

---
+++ src/components/Main.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Category from "./Category";
+import styled from "styled-components";
+import Header from "./Header";
+import ChatLists from "./ChatLists";
+
+const Main = () => {
+  return (
+    <>
+      <BackgroundBox>
+        <Header />
+      <ContainerBox>
+        {/*<Header />*/}
+        <Category />
+        <ChatLists />
+      </ContainerBox>
+      </BackgroundBox>
+    </>
+  );
+};
+
+export default Main;
+
+const BackgroundBox = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #E0E0E0;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const ContainerBox = styled.div`
+  //background-color: #F8F8F8;
+  background: bisque;
+  width: 830px;
+  height: 583px;
+  display: flex;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row; // 디폴트값이니 나중에 삭제
+  border: 1px solid #E1E1E1;
+`
